5a84239fdf2c785d8534cca8f91f5a27a852aa41
Angelo7x <abennici96@gmail.com>
2021-07-30 17:10:51 +0200

    fix + fine esercizio mail

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -4,9 +4,20 @@ var email = ["pippo007@gmail.com" , "pluto007@gmail.com" , "ciccio007@libero.it"
 var emailutente = (prompt("Inserisci la tua email"));
 console.log(emailutente)
 // controllo email inseriita sulla lista disponibile
-if ( emailutente == email ) {
-    alert ( Accesso consentito);
-} else if {
-    alert ( Accesso negato );
+
+var control = 0
+
+for (var i = 0 ; i < email.length -1 ; i++) {
+    if ( emailutente == email[i] ) {
+        control = 1 ;
+    }
 }
+
+if (control == 1) {
+    alert ("Accesso consentito")
+}
+else {
+    alert ("Accesso negato")
+}
+
 // Se l'email inserita è presente nella lista stampare si , altrimenti no
